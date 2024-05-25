ea66b70121d062b91b34fdf1002d20b051914c6e
MuthonduG <muthondugithinji@gmail.com>
2024-05-25 19:36:56 +0300

    alx learning

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    char c;
+
+    /* Print lowercase alphabet */
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+
+    /* Print uppercase alphabet */
+    for (c = 'A'; c <= 'Z'; c++)
+    {
+        putchar(c);
+    }
+
+    /* Print new line */
+    putchar('\n');
+
+    return (0);
+}
