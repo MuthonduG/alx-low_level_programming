3ca0d83487206340acf7b182887e2040163d918e
MuthonduG <muthondugithinji@gmail.com>
2024-05-26 18:13:10 +0300

    alx programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 48; i < 58; i++)
-        putchar(i);
-    for (i = 97; i < 103; i++)
-        putchar(i);
-    putchar(10); /* newline */
-    return (0);
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar(10); /* newline */
+	return (0);
 }
