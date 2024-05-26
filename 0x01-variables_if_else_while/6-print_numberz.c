2db68272a5ab6831e250e52df3157d73dbb6bb15
MuthonduG <muthondugithinji@gmail.com>
2024-05-26 18:01:03 +0300

    alx programming

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,17 +12,7 @@
 
 int main(void)
 {
-    putchar(48); /* ASCII value for '0' */
-    putchar(49); /* ASCII value for '1' */
-    putchar(50); /* ASCII value for '2' */
-    putchar(51); /* ASCII value for '3' */
-    putchar(52); /* ASCII value for '4' */
-    putchar(53); /* ASCII value for '5' */
-    putchar(54); /* ASCII value for '6' */
-    putchar(55); /* ASCII value for '7' */
-    putchar(56); /* ASCII value for '8' */
-    putchar(57); /* ASCII value for '9' */
-    putchar(10); /* ASCII value for '\n' */
+    putchar(48); 
+    putchar(10); 
     return (0);
 }
-
