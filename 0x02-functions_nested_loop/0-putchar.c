b3cbba4d06214c92e2327830638d7e9477845ddb
MuthonduG <muthondugithinji@gmail.com>
2024-05-26 23:02:17 +0300

    alx programming

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Function main has print function
+ * output should be the string _putchar
+*/
+
+int main(void) {
+    /* use printf to print to the console */
+    printf("_putchar\n");
+
+    return 0;
+}
